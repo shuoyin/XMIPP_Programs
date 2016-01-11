03f631643278070a7bb09251ebf096bffe2d4970
yinshuo24 <shuo.yin@intel.com>
2016-01-11 20:41:57 +0800

    add imageio program  using xmipp libraries

---
+++ ImageIO/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lXmippData -lXmippExternal -lXmippDimred -lXmippJNI -lXmippParallel -lXmippRecons -lXmippSqliteExt -ltiff
+
---
+++ ImageIO/Debug/src/main.d
@@ -0,0 +1,247 @@
+src/main.o: ../src/main.cpp \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_image.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_image_base.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_image_macros.h \
+ /home/yinshuo/usr/xmipp/libraries/data/multidim_array.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/bilib/types/tsplinebasis.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/bilib/headers/kernel.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_strings.h \
+ /home/yinshuo/usr/xmipp/libraries/data/matrix1d.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_funcs.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_filename.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_macros.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_error.h \
+ /home/yinshuo/usr/xmipp/libraries/data/numerical_recipes.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_memory.h \
+ /home/yinshuo/usr/xmipp/libraries/data/matrix2d.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/bilib/headers/linearalgebra.h \
+ /home/yinshuo/usr/xmipp/libraries/data/transformations.h \
+ /home/yinshuo/usr/xmipp/external/bilib/headers/kerneldiff1.h \
+ /home/yinshuo/usr/xmipp/external/bilib/types/tboundaryconvention.h \
+ /home/yinshuo/usr/xmipp/external/bilib/headers/changebasis.h \
+ /home/yinshuo/usr/xmipp/external/bilib/headers/../types/tsplinebasis.h \
+ /home/yinshuo/usr/xmipp/external/bilib/headers/../types/tboundaryconvention.h \
+ /home/yinshuo/usr/xmipp/external/bilib/headers/pyramidtools.h \
+ /home/yinshuo/usr/xmipp/libraries/data/multidim_array_generic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_datatype.h \
+ /home/yinshuo/usr/xmipp/libraries/data/geometry.h \
+ /home/yinshuo/usr/xmipp/libraries/data/metadata.h \
+ /home/yinshuo/usr/xmipp/libraries/data/metadata_sql.h \
+ /home/yinshuo/usr/xmipp/external/sqlite-3.6.23/sqlite3.h \
+ /home/yinshuo/usr/xmipp/libraries/data/metadata_label.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/tiff-3.9.4/libtiff/tiffio.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/tiff-3.9.4/libtiff/tiff.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/tiff-3.9.4/libtiff/tiffconf.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/tiff-3.9.4/libtiff/tiffvers.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/hdf5.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5public.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5pubconf.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5version.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5api_adpt.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Apublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Ipublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Opublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Lpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Tpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5ACpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Cpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Dpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Epublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Epubgen.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Fpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Gpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5MMpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Ppublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Zpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Rpublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Spublic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDcore.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDfamily.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDlog.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDmpi.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDmpio.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDmpiposix.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDmulti.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDsec2.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDstdio.h \
+ /home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDdirect.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwDM3.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwIMAGIC.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwMRC.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwINF.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwRAW.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwSPIDER.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwSPE.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwTIA.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwJPEG.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwTIFF.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwEM.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwPIF.h \
+ /home/yinshuo/usr/xmipp/libraries/data/rwHDF5.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_image_generic.h \
+ /home/yinshuo/usr/xmipp/libraries/data/xmipp_color.h
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_image.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_image_base.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_image_macros.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/multidim_array.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/bilib/types/tsplinebasis.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/bilib/headers/kernel.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_strings.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/matrix1d.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_funcs.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_filename.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_macros.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_error.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/numerical_recipes.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_memory.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/matrix2d.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/bilib/headers/linearalgebra.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/transformations.h:
+
+/home/yinshuo/usr/xmipp/external/bilib/headers/kerneldiff1.h:
+
+/home/yinshuo/usr/xmipp/external/bilib/types/tboundaryconvention.h:
+
+/home/yinshuo/usr/xmipp/external/bilib/headers/changebasis.h:
+
+/home/yinshuo/usr/xmipp/external/bilib/headers/../types/tsplinebasis.h:
+
+/home/yinshuo/usr/xmipp/external/bilib/headers/../types/tboundaryconvention.h:
+
+/home/yinshuo/usr/xmipp/external/bilib/headers/pyramidtools.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/multidim_array_generic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_datatype.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/geometry.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/metadata.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/metadata_sql.h:
+
+/home/yinshuo/usr/xmipp/external/sqlite-3.6.23/sqlite3.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/metadata_label.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/tiff-3.9.4/libtiff/tiffio.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/tiff-3.9.4/libtiff/tiff.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/tiff-3.9.4/libtiff/tiffconf.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/tiff-3.9.4/libtiff/tiffvers.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/hdf5.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5public.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5pubconf.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5version.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5api_adpt.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Apublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Ipublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Opublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Lpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Tpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5ACpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Cpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Dpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Epublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Epubgen.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Fpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Gpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5MMpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Ppublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Zpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Rpublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5Spublic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDcore.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDfamily.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDlog.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDmpi.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDmpio.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDmpiposix.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDmulti.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDsec2.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDstdio.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/../../external/hdf5-1.8.10/src/H5FDdirect.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwDM3.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwIMAGIC.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwMRC.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwINF.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwRAW.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwSPIDER.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwSPE.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwTIA.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwJPEG.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwTIFF.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwEM.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwPIF.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/rwHDF5.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_image_generic.h:
+
+/home/yinshuo/usr/xmipp/libraries/data/xmipp_color.h:
